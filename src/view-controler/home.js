6e74424d2ed13f658bb4bd1596fab0a3d2b9fd10
Nextiva challenge resolver <cristinagrl@hotmail.com>
2021-05-26 04:45:50 -0500

    post css

--- src/view-controler/home.js
+++ src/view-controler/home.js
@@ -6,21 +6,25 @@ export default (user) => {
   <nav>
   <a class="navhome" href="#">Log out</a>
   </nav>
-  <form id="task-form">
   <h1>${`${user.firstName} ${user.lastName}`}</h1>
-  <div class="flex-container">
-  <div><textarea id="description" class="post" placeholder="what're you thinking?"></textarea></div>
-  </div>
+  <article class = "create-post">
+  <h2>Share your moments</h2>
+  <div class="img-textPost">
+    <svg height="60" width="80">
+      <circle cx="30" cy="30" r="25"/>
+    </svg>
+  <input type="text" id="textarea" placeholder="what're you thinking?">
+  </input></div>
   <button class="buttonpost" id="post">to share</button>
   <div class="sectionPost" id="sectionPost"></div>
-  </form>
+  </article>
   `;
   const divElem = document.createElement('div');
   divElem.innerHTML = viewPosts;
 
   const postText = divElem.querySelector('#post');
   postText.addEventListener('click', () => {
-    const area = divElem.querySelector('#description').value; // captura lo que ingresa el usuario, valor de la etiqueta
+    const area = divElem.querySelector('#textarea').value; // captura lo que ingresa el usuario, valor de la etiqueta
     addPost(area, user.userId);
   });
 
